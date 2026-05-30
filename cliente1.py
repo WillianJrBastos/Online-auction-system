ad0a1dfcfd8504201d85c7f9f56a38d3f0316f48
WillianJrBastos <wjbastos1@gmail.com>
2026-05-30 13:14:00 -0300

    Implementando design ao terminal

--- cliente1.py
+++ cliente1.py
@@ -1,8 +1,11 @@
 import socket
 
-address = input("Digite o endereço do servidor: ")
-port = int(input("Digite a porta do servidor: "))
-nome = input("Digite seu nome de usuário: ")
+# address = input("Digite o endereço do servidor: ")
+address = 'localhost'
+# port = int(input("Digite a porta do servidor: "))
+port = 12345
+# nome = input("Digite seu nome de usuário: ")
+nome = "Will"
 address_server = (address, port)
 
 cliente1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- servidor.py
+++ servidor.py
@@ -1,13 +1,64 @@
 import socket
 import threading
 
-address = input("Digite o endereço do servidor: ")
-port = int(input("Digite a porta do servidor: "))
-nomeItem = input("Digite o nome do item: ")
-valorInicial = float(input("Digite o valor inicial do lance do item: R$"))
-tempo = int(input("Digite o tempo que ra durar o leilão (em segundos): "))
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich.table import Table
+from rich.layout import Layout
+from rich.align import Align
+from rich.live import Live
+
+# address = input("Digite o endereço do servidor: ")
+address = 'localhost'
+# port = int(input("Digite a porta do servidor: "))
+port = 12345
+# nomeItem = input("Digite o nome do item: ")
+nomeItem = "Moto"
+# valorInicial = float(input("Digite o valor inicial do lance do item: R$"))
+valorInicial = 10000.0
+# tempo = int(input("Digite o tempo que ra durar o leilão (em segundos): "))
+tempo = 30
 clientes = []
+vencedor = "?"
+nomes = {}
+leilao_ativo = False
+
 server_address = (address, port)
+
+lock = threading.Lock()
+
+console = Console()
+
+def criar_layout():
+    layout = Layout()
+
+    layout.split(
+        Layout(name="cabecalho", size=3),
+        Layout(name="corpo", ratio=1),
+        Layout(name="clientes", size=5),
+        Layout(name="rodape", size=5)
+    )
+
+    titulo = Panel(Align.center("Leilão online"), style="bold", expand=True)
+    
+    tabela = Table(expand=True)
+    tabela.add_column("Item", justify="center")
+    tabela.add_column("Lance", justify="center")
+    tabela.add_column("Maior lance de", justify="center")
+    tabela.add_column("Tempo", justify="center")
+    tabela.add_row(nomeItem, f"R${valorInicial}", vencedor, f"{tempo}s")
+
+    clientes = "\n".join(nomes.values()) if nomes else "Nenhum cliente conectado"
+    painel_clientes = Panel(clientes,title="Clientes")
+    info = Panel("Esperando lances...")
+
+    layout["cabecalho"].update(titulo)
+    layout["corpo"].update(tabela)
+    layout["clientes"].update(painel_clientes)
+    layout["rodape"].update(info)
+
+    return layout
 
 def broadcast(mensagem):
     for cliente in clientes:
@@ -20,7 +71,10 @@ def broadcast(mensagem):
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(server_address)
 server.listen()
-print(f"Esperando clientes entrarem no servidor... \n{address}:{port}")
+
+layout = criar_layout()
+
+console.print(layout)
 
 while True:
 
